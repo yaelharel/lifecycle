5acfbc9894d5bebb989137f70027ed7a41cbca18
Yael Harel <yharel@vmware.com>
2020-10-01 10:47:13 -0400

    update error version to unknown
    Signed-off-by: Natalie Arellano <narellano@vmware.com>

--- tools/version/main.go
+++ tools/version/main.go
@@ -13,7 +13,7 @@ func main() {
 	cmd := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD")
 	output, err := cmd.CombinedOutput()
 	if err != nil {
-		fmt.Println("0.0.0")
+		fmt.Println("unknown")
 		return
 	}
 	re := regexp.MustCompile("release/(?P<version>.+)")
@@ -28,13 +28,13 @@ func main() {
 	cmd = exec.Command("git", "describe", "--tags")
 	output, err = cmd.CombinedOutput()
 	if err != nil {
-		fmt.Println("0.0.0")
+		fmt.Println("unknown")
 		return
 	}
 	re = regexp.MustCompile("v(?P<version>.+)-(?P<commits>.+)-g(?P<sha>.+)")
 	matches = re.FindStringSubmatch(string(output))
 	if len(matches) != 4 {
-		fmt.Println("0.0.0")
+		fmt.Println("unknown")
 		return
 	}
 	fmt.Println(matches[1] + "-" + matches[2] + "+" + matches[3])
